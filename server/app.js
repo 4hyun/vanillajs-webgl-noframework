6474ad97fadc9a3c3144f9aeb1cd7b399c38f496
Sa Hyun Hwang <4hyun8@gmail.com>
2022-06-17 09:52:45 +0900

    wip

--- server/app.js
+++ server/app.js
@@ -2,14 +2,24 @@ import express from "express";
 import path from "path";
 const app = express();
 const port = 3002;
-app.set("views", path.join(__dirname, "..", "views"));
+const viewPath = path.join(__dirname, "..", "views");
+app.set("views", viewPath);
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("pages/home");
+});
+app.get("/about", (req, res) => {
+  res.render("pages/about");
+});
+app.get("/zones", (req, res) => {
+  res.render("pages/zones");
+});
+app.get("/detail/:id", (req, res) => {
+  res.render("pages/detail");
 });
 
 app.listen(port, () => {
-  console.log(path.join(__dirname));
+  console.log("debug `viewPath`: ", viewPath);
   console.log("App listening on port " + port + ".");
 });
